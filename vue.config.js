c517fc454467526df148568e7d42b389d3f4df40
Cameron Shannon <cameroncshannon@gmail.com>
2019-03-24 17:08:59 -0700

    Update vue.config.js
    Changed contents for new settings

--- vue.config.js
+++ vue.config.js
@@ -1,21 +1,28 @@
-// const IS_PRODUCTION = process.env.NODE_ENV === 'production'
+const BundleTracker = require("webpack-bundle-tracker");
 
 module.exports = {
-    outputDir: 'dist',
-    assetsDir: 'static',
-    // baseUrl: IS_PRODUCTION
-    // ? 'http://cdn123.com'
-    // : '/',
-    // For Production, replace set baseUrl to CDN
-    // And set the CDN origin to `yourdomain.com/static`
-    // Whitenoise will serve once to CDN which will then cache
-    // and distribute
-    devServer: {
-      proxy: {
-        '/api*': {
-          // Forward frontend dev server request for /api to django dev server
-          target: 'http://localhost:8000/',
-        }
-      }
-    }
-  }
+    baseUrl: "http://0.0.0.0:8080/",
+    outputDir: './dist/',
+
+    chainWebpack: config => {
+
+        config.optimization
+            .splitChunks(false);
+
+        config
+            .plugin('BundleTracker')
+            .use(BundleTracker, [{filename: '../frontend/webpack-stats.json'}]);
+
+        config.resolve.alias
+            .set('__STATIC__', 'static');
+
+        config.devServer
+            .public('http://0.0.0.0:8080')
+            .host('0.0.0.0')
+            .port(8080)
+            .hotOnly(true)
+            .watchOptions({poll: 1000})
+            .https(false)
+            .headers({"Access-Control-Allow-Origin": ["\*"]})
+            }
+        };
